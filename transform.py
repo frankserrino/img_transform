b14d6e4791896056f9af8b5b480bbc280bf4a39c
fserrino <frank@serrino.com>
2016-08-23 14:36:58 -0500

    added the command line args inputs and help messages

--- transform.py
+++ transform.py
@@ -1,17 +1,17 @@
 """
-Created on Mon Aug 22 19:51:38 2016
-based on https://gist.github.com/vinu76jsr/8594481
+Created on Mon Aug 22 19:51:38 2016,
+modified and expanded upon from https://gist.github.com/vinu76jsr/8594481
 """
 
 import os
 from path import path
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("SOURCE")
-parser.add_argument("DEST")
+parser.add_argument("--source", help="the input directory of images")
+parser.add_argument("--dest", help="the destination directory of the transformed images")
 args = parser.parse_args()
-SOURCE_DIRECTORY =  args.SOURCE
-DEST_DIRECTORY = args.DEST
+SOURCE_DIRECTORY =  args.source
+DEST_DIRECTORY = args.dest
   
 d = path(SOURCE_DIRECTORY)
 dest_directory = path(DEST_DIRECTORY)
@@ -24,11 +24,11 @@ def transform():
             file_count += 1
             print "Transforming %s" % i
 
-# ***  between this comment and the one below is the place we place our transformation code
+# ***  between this comment and the one below is the place we place our transformation code ***
 
-            i.copy(dest_directory)
+            i.copy(dest_directory)                   
 
-# ***
+# ***                                       *********                                       ***
 
     print 'Transformed %s files' % file_count
 
